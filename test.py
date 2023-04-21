ad8a6e65765c96f2ad177dcb3ef91bf29a42073c
mgallon235 <125981129+mgallon235@users.noreply.github.com>
2023-04-22 01:25:37 +0200

    pygsheets

---
+++ test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import pygsheets
+print("hello world")
+
+gc = pygsheets.authorize(service_file='/Users/mikelgallo/repos/master_repo/account_credentials.json')
+sh = gc.open_by_key('19qpLpQQKfhSKIuQpQg00ZenFejWNBG9XBiiYZiRXrSM')
+
+sh.title
+sh.worksheets()
+
+
